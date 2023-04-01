9dd34dca0d9bcebea4364db7f4f67484b9a00bf7
zloikot <alx.atv.76@ya.ru>
2023-04-01 15:51:02 +0300

    add click module and edit list comprehension

--- gen.py
+++ gen.py
@@ -1,12 +1,21 @@
 import sys, click
 
-max_number,len_block,column_name = int(sys.argv[1]),int(sys.argv[2]),sys.argv[3]
-extender = '0000000000'
 
-spisok = [(extender+str(i))[-len_block:] for i in range(1,max_number+1)]
+@click.command()
+@click.option("--max_number",default = 10, help = "Максимальное число нумерации")
+@click.option("--column_name", default = "name", help ="Название колонки")
+@click.option("--len_block", default = 5, help="Длина генерируемой последовательности")
 
-with open('temp.txt','w') as temp:
-   temp.write(column_name+'\n')
-   for i in spisok:
-      temp.write(str(i)+'\n')
+# 
+# max_number,len_block,column_name = int(sys.argv[1]),int(sys.argv[2]),sys.argv[3]
+
+def chaos_generator(max_number, len_block, column_name):
+    extender = '0000000000'
+
+    spisok = [(extender+str(i))[-len_block:] for i in range(1,max_number+1)]
+
+    with open('temp.txt','w') as temp:
+        temp.write(column_name+'\n')
+        for i in spisok:
+            temp.write(str(i)+'\n')
 
